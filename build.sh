da045f2f621a9829e0dbb49d2686c316e836755b
Jin Won Cho <somehow@uenginecloud.com>
2014-12-19 16:25:20 +0900

    add haproxy default file

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Install HAProxy
 sudo apt-get install -y haproxy
-sudo mv /etc/haproxy/haproxy.cfg{,.original}
 sudo cp haproxy.cfg /etc/haproxy/haproxy.cfg
+sudo cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.default
 sudo cp haproxy /etc/default/haproxy
 sudo service haproxy start
