79384ce454a88e1f3e9eed6c040aa44e7c8dd1f0
Jin Won Cho <somehow@uenginecloud.com>
2014-12-19 14:58:27 +0900

    init

---
+++ build.sh
@@ -0,0 +1,22 @@
+# Install Java
+sudo apt-get update
+sudo apt-get -y -q install python-software-properties
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+sudo echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
+sudo apt-get -y install oracle-java7-installer
+
+sudo ln -s /usr/lib/jvm/java-7-oracle /usr/local/java
+
+# Install Docker
+curl -sSL https://get.docker.com/ubuntu/ | sudo sh
+
+# Install HAProxy
+sudo apt-get install haproxy
+sudo haproxy /etc/default/haproxy
+mv /etc/haproxy/haproxy.cfg{,.original}
+cp haproxy.cfg /etc/haproxy/haproxy.cfg
+service haproxy start
