f1aad3c01d21cd5836bb1c939d828ffab78e4bbb
somehow-ue <somehow@uengine.org>
2014-12-23 23:11:02 +0900

    fixed script error

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 cp /etc/haproxy/haproxy.cfg.default /etc/haproxy/haproxy.cfg
 
-for i in {1..2}
+for i in 1 2
 do
         docker run -d -P --name node$i jboss > node$i.cid
         sudo docker inspect --format='{{.NetworkSettings.IPAddress}}' $(cat node$i.cid) > node$i.ipaddress
