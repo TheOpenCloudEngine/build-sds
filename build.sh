af58c21494037b51935135e0623550d467e25627
Sang Song <swsong_dev@websqrd.com>
2014-12-30 10:03:37 +0900

    리눅스 배포판이름을 build.sh 스크립트의 파라미터로 전달하도록 수정.

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
+if [ -z "$1" ]; then
+    echo "Please select a guest os type."
+    echo "Usage: build.sh [Guest OS Type]"
+    exit 1;
+fi
+
+GUEST_OS="$1"
+echo "GUEST_OS = $GUEST_OS"
+
 # Install Java
 sudo apt-get update
 sudo apt-get -y -q install python-software-properties
@@ -23,15 +32,15 @@ CURPATH=`pwd`
 
 # Install Wildfly server and Fluentd client agent
 cd $CURPATH/jboss
-sudo sh -x build.sh
+sudo sh -x build.sh "$GUEST_OS"
 
 # Install Fluentd server (log collector)
 cd $CURPATH/collector
-sudo sh -x build.sh
+sudo sh -x build.sh "$GUEST_OS"
 
 # Install PostgreSQL
 cd $CURPATH/postgresql
-sudo sh -x build.sh
+sudo sh -x build.sh "$GUEST_OS"
 
 # Basic Configuration
 cd $CURPATH
--- collector/build.sh
+++ collector/build.sh
@@ -1,1 +1,6 @@
-sudo docker build -t collector .
+if [ -z "$1" ]; then
+    echo "Please select a guest os type."
+    echo "Usage: build.sh [Guest OS Type]"
+    exit 1;
+fi
+sudo docker build -t collector ./"$1"
